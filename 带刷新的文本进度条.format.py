37856a7fa94dac43ab8f77345f117585aa6ad8c7
EileenQueen1 <97656330+EileenQueen1@users.noreply.github.com>
2022-04-05 16:28:40 +0800

    带刷新的文本进度条

---
+++ 带刷新的文本进度条.format.py
@@ -0,0 +1,9 @@
+import time
+scale=30
+print("{:-^16}".format('执行开始'))
+for i in range(scale+1):
+    a,b='**'*i,'..'*(scale-i)
+    c=(i/scale)*100
+    print("\r{:^6.2f}%[{}->{}]".format(c,a,b),end="")
+    time.sleep(0.3)
+print("\n{:-^16}".format('执行结束'))
